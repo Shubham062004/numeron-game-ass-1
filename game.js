e859ba63a8975f091720b2f88ce087af33796822
Shubham062004 <chaurasiaamrita400@gmail.com>
2023-10-19 21:11:26 +0530

    completed

--- game.js
+++ game.js
@@ -23,7 +23,10 @@ let lesser_than = document.getElementById("lesser-than")
 greater_than.addEventListener('click', function check(){
     if (num1 > num2){
         score++ ;
+        localStorage.setItem('score', score)
         generateRandomNumber()
+        clearInterval(interval)
+        startInterval()
     }
     else {
         location.href="./gameover.html"
@@ -34,7 +37,10 @@ greater_than.addEventListener('click', function check(){
 equal_to.addEventListener('click', function check(){
     if (num1 == num2){
         score++ ;
+        localStorage.setItem('score', score)
         generateRandomNumber()
+        clearInterval(interval)
+        startInterval()
     }
     else {
         location.href="./gameover.html"
@@ -45,7 +51,10 @@ equal_to.addEventListener('click', function check(){
 lesser_than.addEventListener('click', function check(){
     if (num1 < num2){
         score++ ;
+        localStorage.setItem('score', score)
         generateRandomNumber()
+        clearInterval(interval)
+        startInterval()
     }
     else {
         location.href="./gameover.html"
@@ -54,3 +63,23 @@ lesser_than.addEventListener('click', function check(){
 })
 
 // Iteration 4: Build a timer for the game
+
+let timerbox = document.getElementById("timer");
+let timer=5;
+let interval;
+function startInterval(){
+    timer=5;
+    interval = setInterval(()=>{
+        timerbox.textContent = timer;
+        timer--;
+
+        if (timer == 0){
+            clearInterval(interval)
+            location.href="./gameover.html"
+        }
+    }, 1000);
+}
+
+console.log("score: ", score)
+startInterval();
+localStorage.setItem("score", score)
